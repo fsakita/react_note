b4e4d6deae91008b09dd540dea20c09e5a21c4c8
Fabio Sakita <fsakita@hotmail.com>
2018-10-15 11:23:49 +0100

    Added Material UI React

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React from 'react'
+
+class App extends React.Component {
+  constructor (props) {
+    super(props)
+    this.state = {}
+  }
+
+  render () {
+    return (
+      <div className='c-app'>
+        React Note
+      </div>
+    )
+  }
+}
+
+export default App
